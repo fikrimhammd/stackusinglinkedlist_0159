9c884be0c01ea3bfad2d52c814634abe49632863
fikrimhammd <fikri.kdi60@gmail.com>
2024-05-11 09:33:51 +0700

    void pop

--- stackusinglinkedlist_0159.cpp
+++ stackusinglinkedlist_0159.cpp
@@ -32,4 +32,12 @@ public:
 		return value;
 	}
 
+	void pop() {
+		if (isEmpty()) {
+			cout << "Stack is empty." << endl;
+		}
+		cout << "Popped value: " << top->data << endl;
+		top = top->next;
+	}
+
 	
